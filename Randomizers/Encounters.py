1dce6c507a5d02eef49e890ab682ed898aced25b
Copycat8110 <84425376+Copycat8110@users.noreply.github.com>
2021-11-26 15:02:42 -0600

    Optional flag to keep legendaries as legendaries

--- Randomizers/Encounters.py
+++ Randomizers/Encounters.py
@@ -8,6 +8,7 @@ diamondEncount = 361824127573837173
 pearlEncount = -9035030829162387677
 
 pathList = [diamondEncount, pearlEncount]
+lengendaries = [144,145,146,150,151,243,244,245,249,250,251,377,378,379,380,381,382,383,384,385,386,480,481,482,483,484,485,486,487,488,491]
 
 # make sure the file gamesettings is in this folder
 # gamesettings is in Dpr/scriptableassets
@@ -15,22 +16,30 @@ src = "gamesettings"
 
 env = UnityPy.load(src)
 
-for obj in env.objects:
-    
-    if obj.path_id in pathList:
-        tree = obj.read_typetree()
+# Legends = 1 will keep legendariy encounters legendary, i.e. Palkia will become Mewtwo or something, not wurmple
+def Encount(legend):
+    for obj in env.objects:
         
-        ##Two encounter tables are named FieldEncountTable_d (diamond) and FieldEncountTable_p (pearl)
-        for area in tree['table']:
-            for key in area.keys():
-                if type(area[key]) != int:
-                    if type(area[key][0]) == dict:
-                        for mon in area[key]:
-                            if mon['monsNo'] != 0:
-                                mon['monsNo'] = random.randint(1,493)
-        #Saves the object tree
-        obj.save_typetree(tree)
-                
+        if obj.path_id in pathList:
+            tree = obj.read_typetree()
+            
+            ##Two encounter tables are named FieldEncountTable_d (diamond) and FieldEncountTable_p (pearl)
+            for area in tree['table']:
+                for key in area.keys():
+                    if type(area[key]) != int:
+                        if type(area[key][0]) == dict:
+                            for mon in area[key]:
+                                if mon['monsNo'] != 0:
+                                    if legend == 1:
+                                        for legsNo in lengendaries:
+                                            if mon['monsNo'] == legsNo:
+                                                mon['monsNo'] = random.choice(lengendaries)
+                                    else:
+                                        mon['monsNo'] = random.randint(1,493)
+            #Saves the object tree
+            obj.save_typetree(tree)
+
+Encount(1)
 # saving an edited file
 # apply modifications to the objects
 # don't forget to use data.save()
