bffac23940deb4d9643c4acd324b7983b8d00814
AKCore <dblegendsmud@gmail.com>
2021-11-29 21:37:36 -0800

    Added Pearl and Diamond Rival Randomizer. He will pick one of the mons you did not.

--- Randomizers/Starters.py
+++ Randomizers/Starters.py
@@ -14,16 +14,44 @@ modPath = "mods/exefs/"
 diamondBuildID = 'D9E96FB92878E3458AAE7E8D31AB32A9'
 pearlBuildID = '3C70CAE153DF0B4F8A7B24C60FD8D0E7'
 
-
+#diamond 
+#turtwig w0, 183
+#chimchar w8, 186
+#Pilup w9, 189
 diamondStarter1Off = '{:08X}'.format(0x01FEAB28)
 diamondStarter2Off = '{:08X}'.format(0x01FEABB4)
 diamondStarter3Off = '{:08X}'.format(0x01FEABB8)
-diamondOffsets = [diamondStarter1Off,diamondStarter2Off,diamondStarter3Off]
+#diamond rival
+#you pick turtwig he pick chimchar w0, 186
+#you pick chimchar he pick pillup w8,189 
+#you Pilup he pick turtwig w9, 183
+diamondStarterRival1Off = '{:08X}'.format(0x01FEAEA8)
+diamondStarterRival2Off = '{:08X}'.format(0x01FEAF34)
+diamondStarterRival3Off = '{:08X}'.format(0x01FEAF38)
 
+#pearl
+#turtwig w0, 183
+#chimchar w8, 186
+#Pilup w9, 189
 pearlStarter1Off =  '{:08X}'.format(0x0238DEC8)
 pearlStarter2Off =  '{:08X}'.format(0x0238DF54)
 pearlStarter3Off =  '{:08X}'.format(0x0238DF58)
-pearlOffsets = [pearlStarter1Off, pearlStarter2Off, pearlStarter3Off]
+
+#pearl rival
+#you pick turtwig he pick chimchar w0, 186
+#you pick chimchar he pick pillup w8,189 
+#you Pilup he pick turtwig w9, 183
+pearlStarterRival1Off = '{:08X}'.format(0x0238E248)
+pearlStarterRival2Off = '{:08X}'.format(0x0238E2D4)
+pearlStarterRival3Off = '{:08X}'.format(0x0238E2D8)
+
+#Change hardcoded pokemon name 
+#AssetAssistant/Message/English
+engMsgPathID = -5307844841844767521
+
+#8-EV_POKESELECT_02 Worddata 6(0 idx) Turtwig
+#8-EV_POKESELECT_03 Worddata 6(0 idx) Chimchar
+#8-EV_POKESELECT_04 Worddata 6(0 idx) Piplup
 
 def build_diamond_ips(starters):
   patch = Patch()
@@ -59,17 +87,28 @@ def RandomizeStarters(text):
     starter1 = "mov w0, #" + hex(mon1)
     starter2 = "mov w8, #" + hex(mon2)
     starter3 = "mov w9, #" + hex(mon3)
-
+    #rival build. 
+    rival1 = "mov w0, #" + hex(mon2)
+    rival2 = "mov w8, #" + hex(mon3)
+    rival3 = "mov w8, #" + hex(mon1)
+    
     #build our asm patch
     patch1 = ks.asm(starter1)
     patch2 = ks.asm(starter2)
     patch3 = ks.asm(starter3)
+    patchR1 = ks.asm(rival1)
+    patchR2 = ks.asm(rival2)
+    patchR3 = ks.asm(rival3)
+    
     text.append("Assembling Patch.")
     #convert to strings. 
     hex1 = ''.join( ['{:02X}'.format(b) for b in patch1[0]] )
     hex2 = ''.join( ['{:02X}'.format(b) for b in patch2[0]] )
     hex3 = ''.join( ['{:02X}'.format(b) for b in patch3[0]] )
-    hexIds = [hex1, hex2, hex3]
+    hex4 = ''.join( ['{:02X}'.format(b) for b in patchR1[0]] )
+    hex5 = ''.join( ['{:02X}'.format(b) for b in patchR2[0]] )
+    hex6 = ''.join( ['{:02X}'.format(b) for b in patchR3[0]] )
+    hexIds = [hex1, hex2, hex3, hex4, hex5, hex6]
     
     text.append("Building Patch files.")
     #build diamond patch file. 
@@ -77,6 +116,9 @@ def RandomizeStarters(text):
     "{} {}".format(diamondStarter1Off, hex1),
     "{} {}".format(diamondStarter2Off, hex2),
     "{} {}".format(diamondStarter3Off, hex3),
+    "{} {}".format(diamondStarterRival1Off, hex4),
+    "{} {}".format(diamondStarterRival2Off, hex5),
+    "{} {}".format(diamondStarterRival3Off, hex6),
     ""]
     #write out diamond patch
     if not os.path.exists(modPath):
@@ -100,6 +142,9 @@ def RandomizeStarters(text):
     "{} {}".format(pearlStarter1Off, hex1),
     "{} {}".format(pearlStarter2Off, hex2),
     "{} {}".format(pearlStarter3Off, hex3),
+    "{} {}".format(pearlStarterRival1Off, hex4),
+    "{} {}".format(pearlStarter2RivalOff, hex5),
+    "{} {}".format(pearlStarter3RivalOff, hex6),
     ""]
     
     #write out pearl patch
