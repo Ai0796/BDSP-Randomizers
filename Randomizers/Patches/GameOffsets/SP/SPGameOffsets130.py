4420cad42331a2bbab1a3dc438fd442e71d7a2a8
hp3721 <44118666+hp3721@users.noreply.github.com>
2022-06-01 07:33:38 -0500

    Update offsets for SP v1.3.0

--- Randomizers/Patches/GameOffsets/SP/SPGameOffsets130.py
+++ Randomizers/Patches/GameOffsets/SP/SPGameOffsets130.py
@@ -3,14 +3,14 @@ class SPGameOffsets130:
         self.buildID = '38F59CBDA2EB9C44B72F94C4D25935A2'
 
         # Starter Pokémon Offsets
-        get_DefaultPokeNoAddress = 0x2CF13D0
+        get_DefaultPokeNoAddress = 0x238C8E0
 
         self.starterGrassOffset = get_DefaultPokeNoAddress + 0xC8
         self.starterFireOffset = self.starterGrassOffset + 0x8C
         self.starterWaterOffset = self.starterFireOffset + 0x4
 
         # Rival Pokémon Offsets
-        get_RivalPokeNoAddress = 0x2CF1750
+        get_RivalPokeNoAddress = 0x238CC60
 
         self.rivalGrassOffset = get_RivalPokeNoAddress + 0xC8
         self.rivalFireOffset = self.rivalGrassOffset + 0x8C
